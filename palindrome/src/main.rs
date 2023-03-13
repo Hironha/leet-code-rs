dc0521902d18f96bc827a5a81af3a7be8bfc77fd
hironha <rafaelhirokato@gmail.com>
2023-03-13 19:34:06 -0300

    :tada: init repository

---
+++ palindrome/src/main.rs
@@ -0,0 +1,46 @@
+mod palindrome {
+    pub fn is_palindrome(num: i32) -> bool {
+        let mut rev: i32 = 0;
+        let mut num_copy = num;
+
+        while num_copy > 0 {
+            rev = rev * 10 + num_copy % 10;
+            num_copy /= 10;
+        }
+
+        num == rev
+    }
+}
+
+#[cfg(test)]
+mod palindrome_test {
+    use super::palindrome;
+
+    #[test]
+    fn should_be_palindrome() {
+        let num_1: i32 = 121;
+        let num_2: i32 = 2332;
+        let num_3: i32 = 123321;
+
+        assert!(palindrome::is_palindrome(num_1));
+        assert!(palindrome::is_palindrome(num_2));
+        assert!(palindrome::is_palindrome(num_3));
+    }
+
+    #[test]
+    fn should_not_be_palindrome() {
+        let num_1: i32 = 123;
+        let num_2: i32 = 122;
+        let num_3: i32 = 1879345;
+
+        assert!(!palindrome::is_palindrome(num_1));
+        assert!(!palindrome::is_palindrome(num_2));
+        assert!(!palindrome::is_palindrome(num_3));
+    }
+}
+
+fn main() {
+    let num: i32 = 1234554321;
+
+    println!("number: {} is palindrome? {}", num, palindrome::is_palindrome(num));
+}
