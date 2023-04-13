97540b656cc78e05473f22544404786bd70d56f0
Hironha <rafaelhirokato@gmail.com>
2023-04-13 20:17:01 -0300

    :sparkles: add first occurrence in string solution

--- first-occurrence-in-string/src/lib.rs
+++ first-occurrence-in-string/src/lib.rs
@@ -3,6 +3,24 @@ pub fn str_str(haystack: String, needle: String) -> i32 {
     haystack.find(&needle).map(|i| i as i32).unwrap_or(-1)
 }
 
+pub fn raw_str_str(haystack: String, needle: String) -> i32 {
+    let mut matches: usize = 0;
+    for (i, _) in haystack.chars().enumerate() {
+        for (j, n) in needle.chars().enumerate() {
+            if n == haystack.chars().nth(i + j).unwrap() {
+                matches += 1;
+            } else {
+                matches = 0;
+                break;
+            }
+        }
+        if matches == needle.len() {
+            return i as i32;
+        }
+    }
+    -1
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -11,14 +29,14 @@ mod tests {
     fn expect_success() {
         let haystack_1 = "sadbutsad".to_string();
         let needle_1 = "sad".to_string();
-        assert_eq!(str_str(haystack_1, needle_1), 0);
+        assert_eq!(raw_str_str(haystack_1, needle_1), 0);
 
         let haystack_1 = "leetcode".to_string();
         let needle_1 = "leeto".to_string();
-        assert_eq!(str_str(haystack_1, needle_1), -1);
+        assert_eq!(raw_str_str(haystack_1, needle_1), -1);
 
         let haystack_1 = "hello".to_string();
         let needle_1 = "ll".to_string();
-        assert_eq!(str_str(haystack_1, needle_1), 2);
+        assert_eq!(raw_str_str(haystack_1, needle_1), 2);
     }
 }
